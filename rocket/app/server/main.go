682cee151fa975ca1002e39f1496bf1930950cde
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-01-08 21:11:00 -0500

    meow

--- rocket/app/server/main.go
+++ rocket/app/server/main.go
@@ -12,10 +12,12 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"gorm.io/gorm"
+
+	//"gorm.io/gorm"
 
 	"github.com/droundy/goopt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	swaggerFiles "github.com/swaggo/files"     // swagger embed files
 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 
@@ -92,6 +94,7 @@ func main() {
   Built on OS     : %s
 `, BuildDate, BuildNumber, LatestCommit, RuntimeVer, BuiltOnOs)
 	goopt.Parse(nil)
+
 	db, err := gorm.Open("mysql", "doadmin:AVNS_nQrjtn8ilVHqYs6xIim@tcp(dbaas-db-7154856-do-user-13260059-0.b.db.ondigitalocean.com:25060)/rocket_development?parseTime=true")
 	if err != nil {
 		log.Fatalf("Got error when connect database, the error is '%v'", err)
